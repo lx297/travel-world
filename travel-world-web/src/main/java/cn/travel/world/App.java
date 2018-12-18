3c015105a2be316a2b3c1de9f476f9d2b43f7f33
liuxin <liuxin01@sunline.cn>
2018-12-18 10:23:33 +0800

    create a simple project

---
+++ travel-world-web/src/main/java/cn/travel/world/App.java
@@ -0,0 +1,13 @@
+package cn.travel.world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
